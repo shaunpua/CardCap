210d421c65471cba06bce56e5982432de9e33e6c
shaunpua <svshaunpuax1@gmail.com>
2021-06-11 01:47:10 +0800

    responsive navbar design

---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react'
+import { IoMenu, IoSearch} from "react-icons/io5";
+
+
+
+function Navbar() {
+
+    const [showLinks, setShowLinks] = useState(false);
+    return (
+        <div className="nav">
+            
+            <div className="nav__left">
+                <img className="nav__logo"src={process.env.PUBLIC_URL + 'logo.png'} /> 
+                <h2 className="nav__title">CardCap</h2>
+                <div className="nav__links" id={showLinks ? "hidden": ""}>
+                    <a href="">Home</a>
+                    <a href="">Rankings</a>
+                    <a href="">Browse</a>
+                    <a href="">About</a>
+                </div>
+            </div>
+            <div className="nav__right">
+                
+                <div className="search">
+                    <IoSearch className="search__icon" size="20px" />
+                    <input type="text" placeholder="Search for a card" className="search__input" />
+                </div>
+                  
+                <button className="nav__search"><IoSearch className="icon__nav" size="30px" /></button>
+
+                <button className="nav__menu" onClick={() => setShowLinks(!showLinks)} ><IoMenu className="icon__nav" size="30px" /></button> 
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Navbar
